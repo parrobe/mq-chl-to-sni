96893f8244d9c69b2fcdeaf19bbe7dd628672eb6
Robert Parker4 <parrobe@uk.ibm.com>
2022-12-09 16:35:40 +0000

    Add Java version of tool

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# mq-chl-to-sni
-A repo containing different versions of a tool to convert an IBM MQ Channel name to the IBM MQ SNI string
+# IBM MQ Channel to SNI
+This repository contains a selection of programs written in different languages that can be compiled and ran to convert an IBM MQ channel name to it's SNI format. This is particularly useful as during TLS communication the TLS Client hello will contain a SNI header that can be used by routers to ensure the encrypted traffic reaches the correct destination without the need to resolve the TLS handshake.
+
+While the standard is the SNI header is set to the hostname the client is attempting to reach, by default IBM MQ uses the SNI header in order to provide its multiple certificates functionality. As such the SNI header is set to the MQ Channel name converted to a hostname. The rules IBM MQ uses to convert an IBM MQ channel name to it's SNI format are [documented here](https://www.ibm.com/docs/en/ibm-mq/latest?topic=requirements-how-mq-provides-multiple-certificates-capability).
+
+If you want to use the SNI header to route IBM MQ traffic then you need to ensure you route off the correct SNI, the tools contained here can help convert an IBM MQ channel name to the SNI format for entry into your router or OpenShift Routes configuration.
+
+Alternatively, from IBM MQ version 9.2.4 CD and 9.3.0 LTS the [OutboundSNI property](https://www.ibm.com/docs/en/ibm-mq/9.2?topic=programs-outboundsni-property) can be used to tell IBM MQ to use a hostname instead of a channel name
+for the SNI header.
+
+## Tool usage
+While the tools are all written in different languages, they all function the same way. The tools take a single parameter as input and this is the channel name you want to convert. Alternatively, if no parameter is given, the tool will prompt you for the channel name.
+
+If an invalid IBM MQ channel name is given then the tool will not convert it. Additionally, if a valid IBM MQ channel name is given but the channel will create an invalid URL then a warning message will be issued but the SNI format will be outputted. This is a known limitation with IBM MQ's use of SNI and so users of IBM MQ who wish route based on the SNI header must ensure that their channel names end with a capital letter.
+
+## Compiling the tools
+
+### Java
+Java does not require any special libraries and be compiled with:
+```
+$ javac mqChannelToSNI.java
+```
+
+## Examples of use
+
+### Java
+Running the java tool supplying the Channel as a parameter:
+```
+$ java -cp . mqChannelToSNI My.Test.CHL
+Converting IBM MQ Channel name: My.Test.CHL
+SNI format is: m79-2e-t65-73-74-2e-chl.chl.mq.ibm.com
+```
+
+Running the java tool without any parameters:
+```
+$ java -cp . mqChannelToSNI
+Enter IBM MQ channel name: Another.channel1
+Converting IBM MQ Channel name: Another.channel1
+SNI format is: a6e-6f-74-68-65-72-2e-63-68-61-6e-6e-65-6c-1.chl.mq.ibm.com
+```
+
+## Health Warning
+These programs are provided as-is with no guarantees of support or updates. There are
+also no guarantees of compatibility with any future versions of IBM MQ .
+
+## Issues
+For feedback and issues relating specifically to this package, please use
+the [GitHub issue tracker](https://github.com/parrobe/mq-chl-to-sni/issues).
+
+## Copyright
+
+Copyright Rob Parker 2022
---
+++ mqChannelToSNI.java
@@ -0,0 +1,142 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/*
+  Copyright (c) IBM Corporation 2022
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at:
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+   Contributors:
+     Rob Parker - Initial Contribution
+*/
+public class mqChannelToSNI {
+
+    private final String IBM_MQ_SNI_SUFFIX = ".chl.mq.ibm.com";
+
+    /**
+     * Main entry point, obtains the IBM MQ Channel name to convert 
+     * and calls the convert function to action.
+     * 
+     * @param args Given inputs
+     */
+    public static void main(String[] args) {
+        String mqChl;
+        mqChannelToSNI run = new mqChannelToSNI();
+
+        if(args.length >= 1){
+            mqChl = args[0];
+        } else {
+            // Prompt for channel name on stdin
+            System.out.print("Enter IBM MQ channel name: ");
+            Scanner stdin = new Scanner(System.in);
+            mqChl = stdin.nextLine();
+            stdin.close();
+        }
+
+        run.ConvertChlToSNI(mqChl);
+    }
+
+    /**
+     * Validates the given string is a valid IBM MQ Channel name,
+     * converts it to the SNI format and then prints out the result
+     * 
+     * @param mqChl The IBM MQ Channel name to convert
+     */
+    public void ConvertChlToSNI(String mqChl) {
+        String sni;
+        
+        if(!validateChannelName(mqChl)) {
+            System.out.println("Error: " + mqChl + " is not valid IBM MQ Channel name");
+            return;
+        }
+
+        if(IssueURLWarning(mqChl)){
+            System.out.println("Warning: " + mqChl + " is a valid IBM MQ Channel name but the SNI generated will not be URL valid.");
+        } else {
+            System.out.println("Converting IBM MQ Channel name: " + mqChl);
+        }
+
+        sni = doConvert(mqChl);
+
+        // Print out result
+        System.out.println("SNI format is: " + sni);
+    }
+
+    /**
+     * Converts the given IBM MQ Channel name string into its SNI 
+     * format as per: https://www.ibm.com/docs/en/ibm-mq/latest?topic=requirements-how-mq-provides-multiple-certificates-capability
+     * 
+     * @param mqChl The IBM MQ  Channel name string to convert
+     * @return The SNI version of the given string
+     */
+    private String doConvert(String mqChl) {
+        String SNI = "";
+        int len = mqChl.length();
+
+        // Iterate through the characters converting them
+        for(int i = 0; i < len; i++) {
+            char c = mqChl.charAt(i);
+            if(Character.isDigit(c)) {
+                SNI += c;
+            } else if(Character.isUpperCase(c)) {
+                SNI += Character.toLowerCase(c);
+            } else {
+                SNI += String.format("%02x-", (int)c);
+            }
+        }
+
+        // Add the suffix
+        SNI += IBM_MQ_SNI_SUFFIX;
+
+        return SNI;
+    }
+
+    /**
+     * As per the note box on https://www.ibm.com/docs/en/ibm-mq/latest?topic=requirements-how-mq-provides-multiple-certificates-capability
+     * some environments are strict on the SNI naming scheme and how IBM MQ converts a channel name to SNI does not conform to this in all
+     * cases. As such if we detect this case we will output a warning.
+     * @param channelname The IBM MQ channel name string to verify
+     * @return Whether to issue the warning or not
+     */
+    private boolean IssueURLWarning(String channelname) {
+        char c = channelname.charAt(channelname.length()-1);
+        if(Pattern.matches("[a-z_\\/%.]", ""+c)){
+            // Found a charcter that will be converted to an invalid URL
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Verifies that a given IBM MQ Channel name is valid per the rules 
+     * set out in https://www.ibm.com/docs/en/ibm-mq/9.3?topic=objects-rules-naming-mq
+     * 
+     * @param channelname The IBM MQ channel name string to verify
+     * @return true if the string is valid or false if not.
+     */
+    private boolean validateChannelName(String channelname) {
+        // The maximum length of a channel name is 20 characters
+        if(channelname.length() > 20  || channelname.length() == 0) {
+            return false;
+        }
+
+        // The allowed characters in a channel name are:
+        // A-Z a-z 0-9 _ / % .
+        if(Pattern.matches("[^A-Za-z0-9_\\/%.]", channelname)){
+            // Found a character not in the above set
+            return false;
+        }
+
+        return true;
+    }
+}
